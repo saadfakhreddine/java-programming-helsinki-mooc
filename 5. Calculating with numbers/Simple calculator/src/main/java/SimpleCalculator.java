cef2beb36fbc20b80145c4dc0f738e357f94e846
Saad Fakhreddine <181595673+saadfakhreddine@users.noreply.github.com>
2025-06-25 22:54:57 +0100

    Answer Simple calculator

---
+++ 5. Calculating with numbers/Simple calculator/src/main/java/SimpleCalculator.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int number1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int number2 = Integer.valueOf(scanner.nextLine());
+        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+        System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));
+        System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
+        System.out.println(number1 + " / " + number2 + " = " + ((double)number1 / number2));
+        
+
+    }
+}
