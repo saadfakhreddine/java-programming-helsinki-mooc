af4b9ad2a29f9e16d59086973e766178d31069ac
Saad Fakhreddine <181595673+saadfakhreddine@users.noreply.github.com>
2025-06-26 23:18:36 +0100

    Answer Speeding Ticket

---
+++ 6. Conditional statements and conditional operation/Speeding Ticket/src/main/java/SpeedingTicket.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SpeedingTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give speed:");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number > 120) {
+            System.out.println("Speeding ticket!");
+        }
+    }
+}
